c4a59ea332834375d4bf6f5694a94cbf233adf0a
Gustavo <mcmeowizzi@gmail.com>
2023-08-30 20:55:55 -0300

    index printando nomes e filtrado

--- index.php
+++ index.php
@@ -12,22 +12,34 @@ include("includes/db.php");
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <form action="" method="get">
+    <form action="index.php" method="get">
     <header>
         <h1>Bem-vindo ao PiscoCoast</h1>
     </header>
     <nav>
         <h2>Lanches</h2>
         <ul>
-            <?php
-            foreach($produtos as $produto) :
-            ?>
-
+            <?php foreach($produtos as $produto) :?>
+                <?php if($produto['tipo']=="lanche") : ?>
+                <div class="box">
+                    <p><?php echo $produto['nome'];?></p>
+                </div>
+                <?php endif?>
+                <?php endforeach ?>
+        </ul>
+            <h2>Bebidas</h2>
+            <ul>
+            <?php foreach($produtos as $produts) : ?>
+            <?php  if($produts['tipo']=="bebida") : ?>
+                <div class="box">
+                    <p><?php echo $produts['nome'];?></p>
+                </div>
+            <?php endif ?>
             <?php endforeach ?>
         </ul>
     </nav>
     <footer>
-        <p>&copy; 2023 PiscoCoast. Todos os direitos reservados.</p>
+        <p>&copy; 2023 Pisco-Coast. Todos os direitos reservados.</p>
     </footer>
     </form>
 </body>
