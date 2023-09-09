6f85a54acddc891f88c5e9091399fbb9238522cf
Gustavo <mcmeowizzi@gmail.com>
2023-09-09 18:28:10 -0300

    Agora não remove todos os produtos com o mesmos ids

--- delete.php
+++ delete.php
@@ -4,18 +4,18 @@ include("includes/db.php");
 
 if (!isset($_SESSION['id'])) {
     header("location: login.php");
-    exit; // Encerrar o script para evitar processamento desnecessário
+    exit;
 }
 
 if (isset($_GET['id_produto'])) {
     $produto_id = $_GET['id_produto'];
     $cliente_id = $_SESSION['id'];
 
-    $sql_delete = "DELETE FROM carrinho WHERE id_cliente = $cliente_id AND id_produto = $produto_id";
+    $sql_delete = "DELETE FROM carrinho WHERE id_cliente = $cliente_id AND id_produto = $produto_id LIMIT 1";
     $result_delete = $conn->query($sql_delete);
 
     if ($result_delete) {
-        header("location: fazer_pedido.php"); // Redirecionar de volta para o carrinho
+        header("location: fazer_pedido.php");
         exit;
     } else {
         echo "Erro ao excluir o produto do carrinho.";
